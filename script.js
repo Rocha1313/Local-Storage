130bedf6a3ec03e59ec199c191d13a7ce8b79b7c
rocha <rjoao0112@gmail.com>
2023-01-26 16:37:00 +0000

    RemoveButton

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log();
+console.clear();
 
 document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
@@ -7,7 +7,7 @@ document.getElementById("form").addEventListener("submit", (e) => {
 const text = document.getElementById("input");
 const add = document.getElementById("add");
 const remAll = document.getElementById("remAll");
-const allButtons = document.getElementsByClassName("buttonStyle");
+let allButtons = document.getElementsByClassName("buttonStyle");
 let list = document.getElementById("list");
 
 let storage = JSON.parse(localStorage.getItem("toDoList"));
@@ -24,31 +24,69 @@ if (storage) {
   }
 }
 
-for (let i = 0; i < allButtons.length; i++) {
-  allButtons[i].addEventListener("click", () => {
-    console.log("yo");
-    const buttonRem = allButtons[i];
-    const parent = buttonRem.parentElement();
+const removeElement = (button) => {
+  button.addEventListener("click", () => {
+    const parent = button.parentElement;
+    console.log(storage.indexOf(button));
+    storage.slice(storage.indexOf(button), 1);
+    localStorage.setItem("toDoList", JSON.stringify(storage));
     parent.remove();
   });
+};
+
+for (let i = 0; i < allButtons.length; i++) {
+  removeElement(allButtons[i]);
 }
 
 add.addEventListener("click", () => {
   storage = JSON.parse(localStorage.getItem("toDoList"));
   list = document.getElementById("list");
+
   if (!storage) {
-    localStorage.setItem("toDoList", JSON.stringify([text.value]));
+    localStorage.setItem(
+      "toDoList",
+      JSON.stringify([
+        {
+          id: "0",
+          text: text.value,
+        },
+      ])
+    );
     const li = document.createElement("li");
     const delButton = document.createElement("button");
     delButton.innerHTML = "x";
     delButton.setAttribute("class", "buttonStyle");
+    delButton.setAttribute("id", "0");
 
     li.innerHTML = text.value;
     list.appendChild(li);
     li.appendChild(delButton);
+
+    removeElement(delButton);
     return;
   }
-  storage.push(text.value);
+  let index;
+
+  for (let i = 0; i <= storage.length; i++) {
+    for (let f = 0; f <= storage.length; f++) {
+      if(!storage[i]){
+        index = storage.length;
+        break
+      }
+      if (f == parseInt(storage[i].id)) {
+        break;
+      }
+    }
+    if (index) {
+      break;
+    }
+  }
+
+  storage.push({
+    id: "" + index,
+    text: text.value,
+  });
+
   localStorage.setItem("toDoList", JSON.stringify(storage));
 
   let li = document.createElement("li");
@@ -59,16 +97,12 @@ add.addEventListener("click", () => {
   li.innerHTML = text.value;
   list.appendChild(li);
   li.appendChild(delButton);
+
+  removeElement(delButton);
 });
 
 remAll.addEventListener("click", () => {
   list = document.getElementById("list");
-  if (list.firstChild) {
-    list.remove();
-    const ul = document.createElement("ul");
-    ul.setAttribute("id", "list");
-    const divList = document.getElementById("listDiv");
-    divList.appendChild(ul);
-    localStorage.clear();
-  }
+  list.innerHTML = "";
+  localStorage.clear();
 });
